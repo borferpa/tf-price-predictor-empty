36c15269448a1ee6cf030b3e4fb721c109a0663f
BORJA ROBERTO FERNANDEZ PAREJA <03744003@l-5cd8416cn0.intranet.mango.es>
2022-02-22 12:53:41 +0100

    feat: add sales by week in Mango

--- src/components/wrapper/wrapper.jsx
+++ src/components/wrapper/wrapper.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
+const salesByWeek = [
+  {
+    sale: 25375162,
+  },
+  {
+    sale: 23927331,
+  },
+  {
+    sale: 22994049,
+  },
+  {
+    sale: 21822741,
+  },
+  {
+    sale: 22781890,
+  },
+  {
+    sale: 19117083,
+  },
+  {
+    sale: 20343253,
+  },
+];
+
 const Wrapper = () => {
   return <>HELLO</>;
 };
